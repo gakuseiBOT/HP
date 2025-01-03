f7e51854cca57be0e0ebbdb328a8c3cfbf267a20
Aki <y.akitaka2009@icloud.com>
2025-01-03 22:34:15 +0900

    GA-ID set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <GoogleAnalytics gaId={"GA-ID"} />
+      <GoogleAnalytics gaId={"G-T218GPX3X5"} />
       <body className="w-full">
         <Header />
         <AppRouterCacheProvider>
