9a5aa28da5759ce3fe2880991fbbc6164c9033a1
yuito-it <yuito@yuito.work>
2025-01-26 23:11:23 +0900

    :hammer: ESlintの設定を更新

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules } from '@eslint/compat';
+import prettier from 'eslint-config-prettier';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const flatCompat = new FlatCompat();
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+const config = [
+  ...fixupConfigRules(
+    flatCompat.extends('next/core-web-vitals'),
+    flatCompat.extends('next/typescript'),
+  ),
+  prettier,
 ];
 
-export default eslintConfig;
+export default config;
