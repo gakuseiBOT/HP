821135a80b52383bcfeaf10647a3308ea541ee3d
i03-Dinar <dinar@vipelar.com>
2025-02-18 12:46:45 +0900

    停止中のコマンドをページ下部に移動 (#67)

--- src/app/howto/page.tsx
+++ src/app/howto/page.tsx
@@ -18,7 +18,131 @@ const ContactsPage: NextPage = () => {
         <h1 className="text-4xl font-bold">使い方</h1>
         <p className="text-lg">gakuseiBotのコマンド一覧です。</p>
       </section>
-      <section id="commands" className="lg:w-2/5 w-full flex flex-col mx-auto space-y-5 p-5">
+      <section
+        id="commands"
+        className="lg:w-2/5 w-full flex flex-col mx-auto space-y-5 p-5"
+      >
+        {/* group-1 */}
+        <CmdCard
+          title="studystart"
+          isRunning={true}
+          description="勉強を開始します。"
+        />
+        <CmdCard
+          title="studyend"
+          isRunning={true}
+          description="勉強を終了します。"
+        />
+        <CmdCard
+          title="serach"
+          isRunning={true}
+          description="ウィキペディアからの引用検索をします。"
+        />
+        <CmdCard
+          title="setgreeting"
+          isRunning={true}
+          description="挨拶メッセージ送信チャンネル指定および機能のオンオフ設定をします。"
+        />
+        <CmdCard
+          title="botinfo"
+          isRunning={true}
+          description="botの導入情報を確認できます。"
+        />
+        <CmdCard
+          title="status"
+          isRunning={true}
+          description="botの起動からの時間を確認できます。"
+        />
+        <CmdCard
+          title="kanji_quiz_read"
+          isRunning={true}
+          description="漢字の読みの問題を出題します。読み書きともに制限時間は30秒。"
+        />
+        <CmdCard
+          title="kanji_quiz_write"
+          isRunning={true}
+          description="漢字の書きの問題を出題します。読み書きともに制限時間は30秒。"
+        />
+        {/* group-2 */}
+        <CmdCard
+          title="math_quiz"
+          isRunning={true}
+          description="数学の問題を出題します。制限時間は45秒です。"
+        />
+        <CmdCard
+          title="ranking"
+          isRunning={true}
+          description="サーバーごとにクイズに正解した回数をランキング形式で表示します。（回数はサーバーごとにカウントされます。）"
+        />
+        <CmdCard
+          title="set_global_chat"
+          isRunning={true}
+          description="グローバルチャットシステムを起動させます。使用したいチャンネルで実行して下さい。"
+        />
+        <CmdCard
+          title="disable_global_chat"
+          isRunning={true}
+          description="グローバルチャットシステムをオフにする際にグローバルチャットシステムを起動したチャンネルで実行して下さい。"
+        />
+        <CmdCard
+          title="add_quote"
+          isRunning={true}
+          description="名言集に名言を追加します。"
+        />
+        <CmdCard
+          title="delete_quote"
+          isRunning={true}
+          description="名言を削除します。"
+        />
+        <CmdCard
+          title="list_quote"
+          isRunning={true}
+          description="サーバー内の名言を一覧表示します。"
+        />
+        <CmdCard
+          title="random_quote"
+          isRunning={true}
+          description="ランダムに名言を表示します。"
+        />
+        <CmdCard
+          title="server_ping"
+          isRunning={true}
+          description="指定したIPアドレスにPINGを送信します。"
+        />
+        <CmdCard
+          title="server_time"
+          isRunning={true}
+          description="アプリの簡単な処理速度を確認します。"
+        />
+        <CmdCard
+          title="server_info"
+          isRunning={true}
+          description="サーバーの詳細な情報を表示します。"
+        />
+        <CmdCard
+          title="check_verified_bots"
+          isRunning={true}
+          description="サーバー内の認証されていないBOTを検出します。"
+        />
+        {/* group-3 */}
+        <CmdCard
+          title="rolepanel"
+          isRunning={true}
+          description="最大20個のロールでロール付与パネルを作成します。"
+        />
+        <CmdCard
+          title="ticket_create"
+          isRunning={true}
+          description="チケットを作成します。"
+        />
+        <CmdCard
+          title="user_info"
+          isRunning={true}
+          description="ユーザーの作成日、警告回数、ブラックリスト、制限状態を表示します。(gakuseibotによる独自のもの)。"
+        />
+        <h1 className="text-2xl font-semibold">停止中のコマンド</h1>
+        {/* ここからisRunnning={false} */}
+        {/* group-1 ここから */}
         <CmdCard
           title="setreminder"
           isRunning={false}
@@ -53,103 +177,22 @@ const ContactsPage: NextPage = () => {
           isRunning={false}
           description="自分のリマインダー一覧を表示します。"
         />
-        <CmdCard title="studystart" isRunning={true} description="勉強を開始します。" />
-        <CmdCard title="studyend" isRunning={true} description="勉強を終了します。" />
-        <CmdCard
-          title="serach"
-          isRunning={true}
-          description="ウィキペディアからの引用検索をします。"
-        />
-        <CmdCard
-          title="setgreeting"
-          isRunning={true}
-          description="挨拶メッセージ送信チャンネル指定および機能のオンオフ設定をします。"
-        />
-        <CmdCard title="botinfo" isRunning={true} description="botの導入情報を確認できます。" />
-        <CmdCard
-          title="status"
-          isRunning={true}
-          description="botの起動からの時間を確認できます。"
-        />
-        <CmdCard
-          title="kanji_quiz_read"
-          isRunning={true}
-          description="漢字の読みの問題を出題します。読み書きともに制限時間は30秒。"
-        />
-        <CmdCard
-          title="kanji_quiz_write"
-          isRunning={true}
-          description="漢字の書きの問題を出題します。読み書きともに制限時間は30秒。"
-        />
+        {/* group-2 ここから */}
         <CmdCard
           title="setreaction"
           isRunning={false}
           description="特定の言葉に特定の言葉で反応します。"
         />
+        {/* group-3 ここから */}
         <CmdCard
-          title="math_quiz"
-          isRunning={true}
-          description="数学の問題を出題します。制限時間は45秒です。"
+          title="setsticky"
+          isRunning={false}
+          description="固定メッセージを設定します。"
         />
-        <CmdCard
-          title="ranking"
-          isRunning={true}
-          description="サーバーごとにクイズに正解した回数をランキング形式で表示します。（回数はサーバーごとにカウントされます。）"
-        />
-        <CmdCard
-          title="set_global_chat"
-          isRunning={true}
-          description="グローバルチャットシステムを起動させます。使用したいチャンネルで実行して下さい。"
-        />
-        <CmdCard
-          title="disable_global_chat"
-          isRunning={true}
-          description="グローバルチャットシステムをオフにする際にグローバルチャットシステムを起動したチャンネルで実行して下さい。"
-        />
-        <CmdCard title="add_quote" isRunning={true} description="名言集に名言を追加します。" />
-        <CmdCard title="delete_quote" isRunning={true} description="名言を削除します。" />
-        <CmdCard
-          title="list_quote"
-          isRunning={true}
-          description="サーバー内の名言を一覧表示します。"
-        />
-        <CmdCard title="random_quote" isRunning={true} description="ランダムに名言を表示します。" />
-        <CmdCard
-          title="server_ping"
-          isRunning={true}
-          description="指定したIPアドレスにPINGを送信します。"
-        />
-        <CmdCard
-          title="server_time"
-          isRunning={true}
-          description="アプリの簡単な処理速度を確認します。"
-        />
-        <CmdCard
-          title="server_info"
-          isRunning={true}
-          description="サーバーの詳細な情報を表示します。"
-        />
-        <CmdCard
-          title="check_verified_bots"
-          isRunning={true}
-          description="サーバー内の認証されていないBOTを検出します。"
-        />
-        <CmdCard title="setsticky" isRunning={false} description="固定メッセージを設定します。" />
         <CmdCard
           title="removesticky"
           isRunning={false}
           description="固定メッセージを解除します。"
-        />
-        <CmdCard
-          title="rolepanel"
-          isRunning={true}
-          description="最大20個のロールでロール付与パネルを作成します。"
-        />
-        <CmdCard title="ticket_create" isRunning={true} description="チケットを作成します。" />
-        <CmdCard
-          title="user_info"
-          isRunning={true}
-          description="ユーザーの作成日、警告回数、ブラックリスト、制限状態を表示します。(gakuseibotによる独自のもの)。"
         />
       </section>
     </main>
