87e02e08de1438c4a5f91072bbbb82e79c81af32
suica-crux <autumn@vipelar.com>
2025-11-06 01:46:25 +0000

    行を消し忘れていたので削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,12 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename)
+const __dirname = dirname(__filename);
 
 const eslintConfig = [
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'next-env.d.ts'],
   },
-  // ...compat.extends('next/core-web-vitals', 'next/typescript')
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
