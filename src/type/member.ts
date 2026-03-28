b146332058418ca8bb59c08651254572ecf6699e
Your Name <your-email@example.com>
2026-03-28 22:04:37 +0900

    feat: メンバーのロールとデータ構造を定義する形を追加

---
+++ src/type/member.ts
@@ -0,0 +1,11 @@
+export type MemberRole = "Owner" | "Engineer" | "Adviser" | "Moderator" | "Creator";
+
+export interface MemberData {
+  name: string;
+  avatar?: string;
+  text: string;
+  homepage?: string;
+  twitter?: string;
+  github?: string;
+  role: MemberRole;
+}
